f12c1d78bb2a64932c6da73c88a7aa45b1d86de6
Sebastian Sünkler <70692981+searchstudies@users.noreply.github.com>
2022-05-06 15:51:06 +0200

    upd

--- tests/install_addon.py
+++ tests/install_addon.py
@@ -1,26 +1,23 @@
 import asyncio
 from playwright.async_api import async_playwright
-from playwright_stealth import stealth_async
 
-url ="https://www.google.de"
 
-user_data_dir = r"C:\Users\SearchLabAdmin\Documents\GitHub\playwright\tests\user-data-dir"
+url ="https://addons.mozilla.org/en-US/firefox/addon/i-dont-care-about-cookies/"
 
-path_to_extension = r"C:\Users\SearchLabAdmin\Documents\GitHub\playwright\tests\dontcare.xpi"
+user_data_dir = r"user-data-dir"
+
+path_to_extension = r'C:\Users\SearchLabAdmin\Documents\GitHub\playwright\tests\dontcare.xpi'
 
 async def main():
     async with async_playwright() as p:
         browser = await p.firefox.launch_persistent_context(
             user_data_dir,
             headless=False,
-            args=[
-                f"--disable-extensions-except={path_to_extension}",
-                f"--load-extension={path_to_extension}",
-            ],
+            
         )
         page = await browser.new_page()
         await page.goto(url, wait_until="load")
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(5000)
         await browser.close()
 
 
